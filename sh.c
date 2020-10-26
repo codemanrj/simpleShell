23069686fe96a5af440c08fdd8a7864df183fe75
Raul Jr. Montano <rjmontan@circinus-44.ics.uci.edu>
2020-10-25 18:05:44 -0700

    Initial skeleton of code provided

---
+++ sh.c
@@ -0,0 +1,88 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+int main(int argc, char **argv)
+{
+  sh_loop();
+  return EXIT_SUCCESS;
+}
+
+void sh_loop(void)
+{
+  char *line;
+  char **args;
+  int status;
+
+  do {
+    printf("143A$ ");
+    line = sh_read_line();
+    args = sh_split_line(line);
+    status = sh_execute(args);
+
+    free(line);
+    free(args);
+  } while (status);
+}
+
+char *sh_read_line(void)
+{
+  char *line = NULL;
+  size_t bufsize = 0;  // have getline allocate a buffer for us
+
+  if (getline(&line, &bufsize, stdin) == -1) {
+    if (feof(stdin))  // EOF
+    {
+      fprintf(stderr, "EOF\n");
+      exit(EXIT_SUCCESS);
+    } else {
+      fprintf(stderr, "Value of errno: %d\n", errno);
+      exit(EXIT_FAILURE);
+    }
+  }
+  return line;
+}
+
+#define SH_TOK_BUFSIZE 64
+#define SH_TOK_DELIM " \t\r\n\a"
+
+char **sh_split_line(char *line)
+{
+  int bufsize = SH_TOK_BUFSIZE;
+  int position = 0;
+  char **tokens = malloc(bufsize * sizeof(char *));
+  char *token, **tokens_backup;
+
+  if (!tokens) {
+    fprintf(stderr, "sh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, SH_TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += SH_TOK_BUFSIZE;
+      tokens_backup = tokens;
+      tokens = realloc(tokens, bufsize * sizeof(char *));
+      if (!tokens) {
+        free(tokens_backup);
+        fprintf(stderr, "sh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, SH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
+}
